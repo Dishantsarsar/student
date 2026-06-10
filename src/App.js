80ae7e474777af1af676d2f57c89b26ac15e7903
deveshsharma <devusharama4707@gmail.com>
2026-06-11 00:40:42 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,8 @@ import Courses from "./Courses";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to Solutions Adda </h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Courses />
     </div>
   );
 }
