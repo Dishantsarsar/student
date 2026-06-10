9142594df047c49ba3d17b88c68f622ac2305979
Prafful Sharma <sharmaprafful646@gmail.com>
2026-06-10 23:12:47 +0530

    h2 tag

--- src/App.js
+++ src/App.js
@@ -6,17 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Welcome to Solutions Adda </h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+ <h2>Dishant boss</h2>
       </header>
     </div>
   );
